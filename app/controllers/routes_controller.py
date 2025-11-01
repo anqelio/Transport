29d9cab9ea7cc0a055dcb14ab67ce0a931a643fc
ankop <bubenkir2007@gmail.com>
2025-11-01 16:35:31 +0500

    router routes

---
+++ app/controllers/routes_controller.py
@@ -0,0 +1,104 @@
+from typing import List, Optional
+from fastapi import Depends, HTTPException, status
+from sqlmodel import Session, select
+from sqlalchemy.exc import IntegrityError
+from app.db.session import get_session
+from app.models.routes import Routes
+
+
+def get_route_by_id(id, session) -> Routes:
+    '''
+    Поиск маршрута по ID
+    :param id:
+    :param session:
+    :return: Routes
+    '''
+    try:
+        result = session.get(Routes, id)
+        if not result:
+            raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail=f"ID не найден")
+        return result
+    except Exception as e:
+        session.rollback()
+        raise HTTPException(status_code=status.HTTP_500_INTERNAL_SERVER_ERROR,
+                            detail=f"Внутренняя ошибка сервера: {str(e)}")
+
+
+def add_route(data, session) -> Optional[Routes]:
+    '''
+    Добавление маршрута
+    :param data:
+    :param session:
+    :return: data
+    '''
+    try:
+        session.add(data)
+        session.commit()
+        session.refresh(data)
+        return data
+    except IntegrityError:
+        session.rollback()
+        raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST,
+                            detail="Ошибка: дубликат или нарушение целостности данных")
+    except Exception as e:
+        session.rollback()
+        raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST,
+                            detail=f"Не удалось добавить остановку, ошибка: {str(e)}")
+
+
+def delete_route_by_id(id: int, session: Session = Depends(get_session)) -> str:
+    '''
+    Удаление маршрута
+    :param data:
+    :param session:
+    :return: str
+    '''
+    try:
+        result = session.get(Routes, id)
+        if not result:
+            raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail=f"ID не найден")
+        session.delete(result)
+        session.commit()
+        return "Удаление выполнено"
+    except Exception as e:
+        session.rollback()
+        raise HTTPException(status_code=status.HTTP_500_INTERNAL_SERVER_ERROR,
+                            detail=f"Внутренняя ошибка сервера: {str(e)}")
+
+
+def update_route(id, data, session) -> Routes:
+    '''
+    Изменение маршрута
+    :param data:
+    :param session:
+    :return: Routes
+    '''
+    try:
+        result = session.get(Routes, id)
+        if not result:
+            raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail=f"ID не найден")
+        for key, value in data.dict(exclude_unset=True).items():
+            setattr(result, key, value)
+        session.commit()
+        session.refresh(result)
+        return result
+    except Exception as e:
+        session.rollback()
+        raise HTTPException(status_code=status.HTTP_500_INTERNAL_SERVER_ERROR,
+                            detail=f"Внутренняя ошибка сервера: {str(e)}")
+
+
+def show_routes(session) -> List[Routes]:
+    '''
+    Вывод информации по маршрутам
+    :param session:
+    :return: List[Routes]
+    '''
+    try:
+        sql = select(Routes)
+        result = session.exec(sql).all()
+        return result
+    except Exception as e:
+        session.rollback()
+        raise HTTPException(status_code=status.HTTP_500_INTERNAL_SERVER_ERROR,
+                            detail=f"Внутренняя ошибка сервера: {str(e)}")
--- app/main.py
+++ app/main.py
@@ -4,6 +4,8 @@ from fastapi import FastAPI
 from app.models import *
 from app.db.database import *
 from app.api.v1.router_transport_type import router as router_transport_v1
+from app.api.v1.router_stops import router as router_stops_v1
+from app.api.v1.router_routes import router as router_route_v1
 
 main_app = FastAPI()
 @asynccontextmanager
@@ -28,4 +30,5 @@ main_app.mount('/api/v1/', app_v1)
 
 
 app_v1.include_router(router_transport_v1, prefix="/transports", tags=['transports'])
-app_v1.include_router(router_transport_v1, prefix="/stops", tags=['stops'])
+app_v1.include_router(router_stops_v1, prefix="/stops", tags=['stops'])
+app_v1.include_router(router_route_v1, prefix="/routes", tags=['routes'])
